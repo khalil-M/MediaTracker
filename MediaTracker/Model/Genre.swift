259e5cd9e8e53284f211b427f0d1d3d8b91598f2
User <khalil.mhelheli@gmail.com>
2025-05-15 13:14:38 +0200

    Created Movie model

---
+++ MediaTracker/Model/Genre.swift
@@ -0,0 +1,37 @@
+//
+//  Genre.swift
+//  MediaTracker
+//
+//  Created by User on 15/05/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Movie {
+    var title: String
+    var genre: Genre
+    
+    init(title: String, genre: Genre) {
+        self.title = title
+        self.genre = genre
+    }
+}
+
+extension Movie {
+    @MainActor
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: Movie.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        container.mainContext.insert(Movie(title: "Jhon Wick", genre: Genre(rawValue: 1)!))
+        container.mainContext.insert(Movie(title: "Groundhok Day", genre: Genre(rawValue: 2)!))
+        container.mainContext.insert(Movie(title: "Toy Story", genre: Genre(rawValue: 7)!))
+        container.mainContext.insert(Movie(title: "Blade Runner", genre: Genre(rawValue: 9)!))
+        container.mainContext.insert(Movie(title: "Lord fo the Rings", genre: Genre(rawValue: 6)!))
+        container.mainContext.insert(Movie(title: "Magnificient Seven", genre: Genre(rawValue: 12)!))
+        container.mainContext.insert(Movie(title: "The revenent", genre: Genre(rawValue: 5)!))
+        container.mainContext.insert(Movie(title: "Dirty Dancing", genre: Genre(rawValue: 10)!)) 
+        return container
+    }
+}
