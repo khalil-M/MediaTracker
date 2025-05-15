2cdf78703c0a2e08b4652ceb11dd3fc8449745fe
User <khalil.mhelheli@gmail.com>
2025-05-15 12:37:00 +0200

    Initial commit: Xcode project setup

---
+++ MediaTracker/MediaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaTrackerApp.swift
+//  MediaTracker
+//
+//  Created by User on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MediaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
