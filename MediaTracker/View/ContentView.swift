4199eba707296b06a404ac82de83b6e1dc05b085
User <khalil.mhelheli@gmail.com>
2025-05-15 15:42:20 +0200

    handle the deletion of item

--- MediaTracker/View/ContentView.swift
+++ MediaTracker/View/ContentView.swift
@@ -12,20 +12,53 @@ struct ContentView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query private var movies: [Movie]
+    
+    @State private var isSheerPresented: Bool = false
+    
     var body: some View {
         List {
-            
+            ForEach(movies) { movie in
+                HStack {
+                    Text(movie.title)
+                    Spacer()
+                    Text(movie.genre.name)
+                }
+                .swipeActions {
+                    Button(role: .destructive) {
+                        withAnimation {
+                            modelContext.delete(movie)
+                        }
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                }
+            }
         }
         .overlay {
             if movies.isEmpty {
                 EmptyListView()
             }
         }
+        .safeAreaInset(edge: .bottom, alignment: .center) {
+            
+            Button {
+                isSheerPresented.toggle()
+            } label: {
+                ButtonImageView(symbolName: "plus.circle.fill")
+            }
+        }
+        .sheet(isPresented: $isSheerPresented) {
+            NewMovieFormView()
+        }
     }
 }
 
-#Preview("Emppty List") {
+#Preview("Sample Data") {
+    ContentView()
+        .modelContainer(Movie.preview)
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Movie.self, inMemory: true)
-    
 }
